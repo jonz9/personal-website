801b926d653fe96628968f565a8fcd2d75ea41cd
John Zhang <zhangjohn.ca@gmail.com>
2024-07-11 21:01:51 -0400

    experience page commented out for now

--- app/experience/page.tsx
+++ app/experience/page.tsx
@@ -1,20 +1,20 @@
-import { experienceData, navItems } from "@/data";
-import { Navbar } from "@/components/Navbar";
-import React from "react";
-import Footer from "@/components/Footer";
-import ExperienceTimeline from "@/components/ExperienceTimeline";
-import PageTransition from "@/components/PageTransition";
+// import { experienceData, navItems } from "@/data";
+// import { Navbar } from "@/components/Navbar";
+// import React from "react";
+// import Footer from "@/components/Footer";
+// import ExperienceTimeline from "@/components/ExperienceTimeline";
+// import PageTransition from "@/components/PageTransition";
 
-const Experience = () => {
-  return (
-    <main className="relative overflow-hidden mx-auto sm:px-10 px-5 justify-center items-center flex flex-col min-h-screen">
-      <Navbar navItems={navItems} className="text-black" />
-      <PageTransition>
-        <ExperienceTimeline experienceItems={experienceData} />
-        <Footer />
-      </PageTransition>
-    </main>
-  );
-};
+// const Experience = () => {
+//   return (
+//     <main className="relative overflow-hidden mx-auto sm:px-10 px-5 justify-center items-center flex flex-col min-h-screen">
+//       <Navbar navItems={navItems} className="text-black" />
+//       <PageTransition>
+//         <ExperienceTimeline experienceItems={experienceData} />
+//         <Footer />
+//       </PageTransition>
+//     </main>
+//   );
+// };
 
-export default Experience;
+// export default Experience;
