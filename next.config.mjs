2d146d4971dd704082e3eff159cfee3e52463361
John Zhang <zhangjohn.ca@gmail.com>
2024-07-12 11:42:36 -0400

    reconfigured next.config.mjs file for successful github pages deployment

--- next.config.mjs
+++ next.config.mjs
@@ -2,6 +2,8 @@ import { withSentryConfig } from "@sentry/nextjs";
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  basePath: "/personal-website",
+  output: "export",
   images: {
     domains: ["api.microlink.io"],
   },
